fc36f7f3ee4ef7d7a6368aa10e9d66c9b31c0c11
Piotr Zych <piotrzych98@gmail.com>
2021-12-30 10:09:21 +0100

    Add cellbase class to the project

---
+++ saper/src/cellbase.py
@@ -0,0 +1,14 @@
+# A base cell class.
+class CellBase:
+
+    def get_x(self):
+        pass
+
+    def get_y(self):
+        pass
+
+    def set_x(self, x):
+        pass
+
+    def set_y(self, y):
+        pass
