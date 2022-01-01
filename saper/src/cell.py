ab2d9c2a8eff818d89e331368324d94a6c8e8639
Piotr Zych <piotrzych98@gmail.com>
2022-01-01 16:04:19 +0100

    Add mark and set_bomb function into Cell class

--- saper/src/cell.py
+++ saper/src/cell.py
@@ -10,5 +10,43 @@ class CellState:
 
 
 class Cell(CellBase):
-    """ The cell on the board. It has its state and other properties.
-     """
+    """
+    The cell on the board. It has its state and other properties.
+    """
+    state = CellState.NOT_CLICKED
+    value = 0
+    is_bomb = False
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def set_state(self, state):
+        self.state = state
+
+    """
+    Right mouse click
+    """
+
+    def mark(self):
+        if self.state == CellState.SURE_BOMB:
+            self.state = CellState.MAYBE_BOMB
+        elif self.state == CellState.MAYBE_BOMB:
+            self.state = CellState.NOT_CLICKED
+        else:
+            self.state = CellState.SURE_BOMB
+
+    def set_bomb(self):
+        self.is_bomb = True
