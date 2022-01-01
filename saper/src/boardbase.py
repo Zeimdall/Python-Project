d82d670e38b04b15c55aa013407855b165a14f0c
Piotr Zych <piotrzych98@gmail.com>
2022-01-01 16:55:50 +0100

    Add create_board, get_cell and bomb functions to class BoardBase

--- saper/src/boardbase.py
+++ saper/src/boardbase.py
@@ -1,3 +1,8 @@
+import random
+
+from cell import Cell, CellState
+
+
 class BoardBase:
     """ A base board game with random generated bombs.
     """
@@ -23,3 +28,25 @@ class BoardBase:
             raise Exception('Number of bombs is not valid')
 
         return True
+
+    def get_cell(self, x, y) -> Cell:
+        return self.board[x][y]
+
+    def update_total_set_as_bomb(self):
+        self.total_set_as_bomb = 0
+
+        for x in range(self.n):
+            for y in range(self.m):
+                cell = self.get_cell(x, y)
+                if cell.is_state_sure_bomb():
+                    self.total_set_as_bomb += 1
+
+    def create_board(self):
+        if not self.is_board_valid():
+            raise ValueError('Board is not valid.')
+
+        self.board = dict()
+
+        for i in range(self.n):
+            self.board[i] = [Cell(i, j) for j in range(self.m)]
+
